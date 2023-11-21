eaf756212a325b3cc48f44460f4a6a3c4d3208f5
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2023-11-21 10:43:58 +0545

    updated mailto

--- src/components/bottomAside.tsx
+++ src/components/bottomAside.tsx
@@ -1,6 +1,11 @@
 export default function BottomAside() {
 	return (
-		<a href='mailto::infoujjwalbhandari@gmail.com' className='bottomaside'>
+		<a
+			href='mailto::infoujjwalbhandari@gmail.com'
+			target='_blank'
+			aria-label='link for mail to ujjwal bhandari'
+			className='bottomaside'
+		>
 			<span className='text-lightGray hover:text-lightGreenBlue'>
 				--&gt; infoujjwalbhandari@gmail.com
 			</span>
