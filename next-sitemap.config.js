b7d9c48a76bac4efc871cc00c52a300ed337f660
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2023-11-22 01:36:20 +0545

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.WEBSITE_URL || 'https://ujjwall.com.np',
+	siteUrl: process.env.WEBSITE_URL || 'https://www.ujjwall.com.np',
+
 	generateRobotsTxt: true,
+	generateIndexSitemap: false,
+	exclude: [
+		'/public/FiraCode',
+		'/public/Tomato_Grotesk',
+		'/public/works',
+		'/public/bestCursor.png',
+		'/public/dp.jpg',
+		'/public/hover-white.png',
+		'/public/navlogo.png',
+		'/public/online.png',
+	],
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
 				<Toaster />
 				<Header />
 				<Cursor />
-				{children}
+				<main>{children}</main>
 				<Footer />
 			</body>
 		</html>
