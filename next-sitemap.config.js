088f42e4e556c8cf92a29712a0586de4a4ed3513
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2023-11-30 19:55:05 +0545

    changed website sitemaps and url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.WEBSITE_URL || 'https://www.ujjwall.com.np',
+	siteUrl: process.env.WEBSITE_URL || 'https://www.ujjwalbhandarii.com.np',
 
 	generateRobotsTxt: true,
 	generateIndexSitemap: false,
