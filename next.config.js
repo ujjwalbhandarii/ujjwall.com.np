236e54dd1a1a4c23c8c5e38110d9ae7e988c063c
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-12-29 22:38:04 +0545

    refactor: pwa reconfigured

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'production' ? true : false,
+  dest: 'public/pwa',
+  // disable: process.env.NODE_ENV === 'production' ? true : false,
 });
 
 module.exports = withPWA({
