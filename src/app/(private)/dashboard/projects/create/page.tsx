198be73dfb2655374286cfecf2b1a2ca2a276975
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2025-01-08 20:16:08 +0545

    chore: removed unused features

--- src/app/(private)/dashboard/projects/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from '@/components/utils/container';
-
-export default function CreateProject() {
-  return (
-    <>
-      <Container>Create a project.</Container>
-    </>
-  );
-}
